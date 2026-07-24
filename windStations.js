81f90d7c050499ea90fd8195792ba3a7729e2377
dme-6100 <dagmagnus.eriksen@brunvoll.no>
2026-07-24 08:55:58 +0200

    Update windStations.js

--- windStations.js
+++ windStations.js
@@ -1,5 +1,4 @@
 // windStations.js
-console.log("WINDSTATIONS VERSION GISKEBRUA");
 
 export const windStations = [
 
@@ -11,16 +10,6 @@ export const windStations = [
     sourceId: "SN60990",
     lat: 62.5667,
     lng: 6.1167,
-    active: true
-  },
-
-  {
-    id: "orsta-volda",
-    name: "Ørsta/Volda",
-    source: "frost",
-    sourceId: "SN59680",
-    lat: 62.1800,
-    lng: 6.0795,
     active: true
   },
 
@@ -44,7 +33,6 @@ export const windStations = [
     active: true
   },
 
- /*
   // Statens vegvesen
   {
     id: "giskebrua",
@@ -54,9 +42,96 @@ export const windStations = [
     lat: 62.5082,
     lng: 6.0763,
     active: true
+  },
+
+  {
+    id: "rundebrua",
+    name: "Rundebrua",
+    source: "vegvesen",
+    stationId: 1823,
+    lat: 62.3793,
+    lng: 5.6275,
+    active: true
+  },
+
+  {
+    id: "nerlandsoybrua",
+    name: "Nerlandsøybrua",
+    source: "vegvesen",
+    stationId: 3508189,
+    lat: 62.3402,
+    lng: 5.6058,
+    active: true
+  },
+
+  {
+    id: "heroybrua",
+    name: "Herøybrua",
+    source: "vegvesen",
+    stationId: 1822,
+    lat: 62.3195,
+    lng: 5.7209,
+    active: true
+  },
+
+  {
+    id: "lepsoybrua",
+    name: "Lepsøybrua",
+    source: "vegvesen",
+    stationId: 550009,
+    lat: 62.5948,
+    lng: 6.2537,
+    active: true
+  },
+
+  {
+    id: "solavagen-ferjekai",
+    name: "Solavågen",
+    source: "vegvesen",
+    stationId: 1860,
+    lat: 62.4135,
+    lng: 6.3271,
+    active: true
+  },
+
+  {
+    id: "gjemnessundbrua",
+    name: "Gjemnessundbrua",
+    source: "vegvesen",
+    stationId: 1687,
+    lat: 62.9712,
+    lng: 7.7794,
+    active: true
+  },
+
+  {
+    id: "bjorndalsbrua",
+    name: "Bjørndalsbrua",
+    source: "vegvesen",
+    stationId: 3508117,
+    lat: 62.2752,
+    lng: 6.0115,
+    active: true
+  },
+
+  {
+    id: "verjeskiftbrua",
+    name: "Verjeskiftbrua",
+    source: "vegvesen",
+    stationId: 3508020,
+    lat: 63.4780,
+    lng: 7.9934,
+    active: true
+  },
+
+  {
+    id: "tresfjordbrua",
+    name: "Tresfjordbrua",
+    source: "vegvesen",
+    stationId: 1651,
+    lat: 62.6113,
+    lng: 7.1126,
+    active: true
   }
-    */
 
 ];
-
-console.log("ANTALL:", windStations.length);
